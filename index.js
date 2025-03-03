0fa6ab47db5c6b9e3f4c4295a88337f9e69a37ed
Ashton Sims <ashtonsims0425@gmail.com>
2025-03-03 10:21:06 +0000

    Completed and updated UI

--- index.js
+++ index.js
@@ -1,93 +1,42 @@
-//identify choices available 
+// LOGIC
+let humanScore = 0
+let computerScore = 0
+let choices = ['rock', 'paper', 'scissors']
 
-const options = ["rock", "paper", "scissors"]
+const computerChoice = () => choices[Math.floor(Math.random() * choices.length)]
 
-//collect computer choice 
+const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice == computerChoice) {
+        return `It's a tie! The computer chose ${computerChoice} and the human chose ${humanChoice}. The computer has won ${computerScore} rounds and the human has won ${humanScore} rounds.`
 
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice; 
-}
-
-//collect human choice
-
-function getPlayerChoice(){
-    let validatedInput = false; 
-    while (validatedInput == false){
-        const choice = prompt("Rock, Paper or Scissors?");
-        if(choice == null){
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        if(options.includes(choiceInLower))
-            validatedInput = true; 
-            return choiceInLower; 
-    }
-}
-
-//check the winner 
-
-function checkWinner(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return "Tie";
-    }
-    else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
+    } else if (
+        (humanChoice == 'rock' && computerChoice == 'scissors') ||
+        (humanChoice == 'paper' && computerChoice == 'rock') ||
+        (humanChoice == 'scissors' && computerChoice == 'paper') 
     ){
-        return "Player";
-    }
-    else{
-        return "Computer";
-    }
+        humanScore++
+        return `Human won! The computer chose ${computerChoice} and the human chose ${humanChoice}. The computer has won ${computerScore} round${computerScore == 1 ? '': `s`} and the human has won ${humanScore} round${humanScore == 1 ? '': `s`}.`
+
+    } else {
+        computerScore++
+        return `Computer won! The computer chose ${computerChoice} and the human chose ${humanChoice}. The computer has won ${computerScore} round${computerScore == 1 ? '': `s`} and the human has won ${humanScore} round${humanScore == 1 ? '': `s`}.`
+    }    
 }
 
-//write a function to play a single round
+// UI
+let buttons = document.querySelectorAll(".btn")
+let score = document.querySelector(".score")
 
-function playRound(playerSelection, computerSelection){
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == 'Tie'){
-        return "It's a Tie!";
-    }
-    else if (result == "Player"){
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`; 
-    }
-}
+const updateUI = result => {
+    score.textContent = `${result}`
+    if(humanScore == 5 || computerScore == 5) {
+        buttons.forEach(button => button.disabled = true) 
+}}
 
-//creating a 5 round game
-
-function game(){
-    let scorePlayer = 0;
-    let scoreComputer = 0; 
-    console.log("Welcome!")
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice(); 
-        const computerSelection = getComputerChoice(); 
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("----------"); 
-        if (checkWinner(playerSelection, computerSelection) == "Player"){
-        scorePlayer ++;
-        }
-        else if (checkWinner(playerSelection, computerSelection) == "Computer"){
-            scoreComputer ++; 
-        }
-    }
-    console.log("Game Over")
-    if (scorePlayer > scoreComputer){
-        console.log("You are the ultimate winner!")
-    }
-    else if (scorePlayer < scoreComputer){
-        console.log("You are the ultimate loser!")
-    }
-    else{
-        console.log("We have a tie!")
-    }
-
-}
-
-game() 
-
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const hChoice = button.textContent.toLowerCase()
+        const cChoice = computerChoice()
+        const result = playRound(hChoice, cChoice)
+        updateUI(result)
+        })})
